1e82679e1352df4221c478b8a22929f51a575dc9
scottbarclay <scottdbarclay@gmail.com>
2018-02-01 19:27:02 -0800

    Add really did bare minimum this time

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,44 +6,44 @@ class App extends React.Component {
       allVideos: window.exampleVideoData,
       seachText: 'aaaaa'
     };
-    this.changeSearchText = this.changeSearchText.bind(this);
-    this.youtubeSearch = _.debounce(this.youtubeSearch.bind(this), 1000); //debouncer
-    this.onTitleClick = this.onTitleClick.bind(this);
+    this.changeSearchText = this.changeSearchText.bind(this); // varibles to refer to bound functions
+    this.youtubeSearch = _.debounce(this.youtubeSearch.bind(this), 500); //debouncer
+    this.onTitleClick = this.onTitleClick.bind(this); // more varibles...
     this.callYouTube = this.callYouTube.bind(this);
   }  
 
-  onTitleClick(video) {
+  onTitleClick(video) { // updates state of video player when a link in the video list is clicked
     this.setState({
       playerVideo: video
     });
   }
 
-  callYouTube(data) {
+  callYouTube(data) { // callback for the ajax call to API; updates state of all videos on the page with data pulled from youtube
     this.setState({
       allVideos: data, 
       playerVideo: data[0]
     });
   }  
   
-  changeSearchText(searchInput) {
+  changeSearchText(searchInput) { // performed on change to search field input
     this.setState({
       searchText: searchInput.target.value
     });
   }
-  youtubeSearch() {
+  youtubeSearch() { // prepares the options package to pass in the ajax call to the API
     var searchOptions = {};
-    searchOptions.query = this.state.searchText;
+    searchOptions.query = this.state.searchText; // sets query text to the current search input text
     searchOptions.max = 5;
-    searchOptions.key = window.YOUTUBE_API_KEY; 
-    this.props.searchYouTube(searchOptions, this.callYouTube);
+    searchOptions.key = window.YOUTUBE_API_KEY; // refers to API creds
+    this.props.searchYouTube(searchOptions, this.callYouTube); // invocation of ajax call function
   }
 
-  render(props) {
+  render(props) { // html that will be inserted into the #app div in index.html
     return (
       <div>
         <nav className="navbar">
           <div className="col-md-6 offset-md-3">
-            <Search changeSearchText={this.changeSearchText} searchButtonClick={this.youtubeSearch}/>
+            <Search changeSearchText={this.changeSearchText} searchButtonClick={this.youtubeSearch}/> 
           </div>
         </nav>
         <div className="row">
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,6 @@
 var Search = (props) => ( // listens for change in search input text and click of search button
   <div className="search-bar form-inline">
-    <input onChange={(searchInput) => props.changeSearchText(searchInput)} className="form-control" type="text" />
+    <input onChange={(searchInput) => { props.changeSearchText(searchInput); props.searchButtonClick(); }} className="form-control" type="text" />
     <button onClick={() => props.searchButtonClick()} className="btn hidden-sm-down">
       <span className="glyphicon glyphicon-search"></span>
     </button>
